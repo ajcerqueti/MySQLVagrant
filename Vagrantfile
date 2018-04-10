895754feebe9debf2bad220b9eb3313dba68db9a
Aj Cerqueti <aj@cerqueti.co.uk>
2018-04-10 17:29:57 +0100

    Move to 16.04

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,23 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
+    # Hostname
     config.vm.hostname = "mysql"
-    config.vm.box = "ubuntu/wily32"
+
+    # Box OS
+    config.vm.box = "ubuntu/xenial64"
+
+    # Enable ports
     config.vm.network :forwarded_port, guest: 3306, host: 3306, host_ip: "127.0.0.1"
+
+	# Create a shared folder
+    # config.vm.synced_folder "./vagrant", "/vagrant", :mount_options => ["dmode=777","fmode=666"]
+
+    # Configure VirtualBox
+    config.vm.provider "virtualbox" do |machine|
+        machine.memory = 1024
+        machine.name = "mysql"
+    end
+
     config.vm.provision :shell, :path => "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
 # Update apt
+echo -e "Updating packages...\n"
 sudo apt-get update
+echo -e "Done\n"
 
 # Set defaults
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password \"''\""
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password \"''\""
+echo -e "Setting passwords... "
+DEFAULTPASS="password"
+sudo debconf-set-selections <<EOF
+mysql-server    mysql-server/root_password password $DEFAULTPASS
+mysql-server    mysql-server/root_password_again password $DEFAULTPASS
+dbconfig-common dbconfig-common/mysql/app-pass password $DEFAULTPASS
+dbconfig-common dbconfig-common/mysql/admin-pass password $DEFAULTPASS
+dbconfig-common dbconfig-common/password-confirm password $DEFAULTPASS
+dbconfig-common dbconfig-common/app-password-confirm password $DEFAULTPASS
+EOF
+echo -e "Done\n"
 
-# Install
-sudo apt-get -y install mysql-server emacs24-nox
+# Install MySQL
+echo -e "Installing MySQL... "
+sudo apt-get install mysql-server-5.7 mysql-client emacs24-nox --assume-yes --force-yes
+echo -e "Done\n"
 
-# Configure
-sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/mysql.conf.d/mysqld.cnf
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+# Add privileges
+echo -e "Granting permission to the host machine... "
+mysql -u root --password=$DEFAULTPASS -e "grant all privileges on *.* to 'root'@'%' identified by '$DEFAULTPASS' with grant option; flush privileges;"
+sudo cp /vagrant/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/
+echo -e "Done\n"
 
-# Restart
+# Restart MySQL
+echo -e "Restarting MySQL Service... "
 sudo service mysql restart
+echo -e "Done\n"
+
+echo -e "Installation complete!\n\n"
